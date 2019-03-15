e03b0152f542b463c07f87282e3e78e16ddce068
yanlei <yanlei6666@gmail.com>
2019-03-15 19:42:02 +0800

    change files

---
+++ alias.bash
@@ -0,0 +1,4 @@
+alias CVV=' pkg-config --modversion opencv;pkg-config --cflags opencv;pkg-config --libs opencv ' 
+alias UPDATESW='sudo apt-get update'
+alias UPDATESWProxy='sudo proxychains apt-get update'
+
